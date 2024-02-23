a4af5fe3c2f8b44b081d41bff29f75d7964dd12e
Brahxter <142146244+Brahxter@users.noreply.github.com>
2024-02-23 16:34:19 +0000

    modified:   makesnosense.py

--- makesnosense.py
+++ makesnosense.py
@@ -11,7 +11,7 @@ class WhimsicalToRealWallet:
 
     def __init__(self):
         # Initialize the Coinbase client with your API key and secret
-        self.client = coinbase.wallet.client.Client(api_key='3c0f8e2c-50c2-462a-b0fb-ff02e8b45331/apiKeys/7caa9d5f-f34c-4dde-be95-5409a7d94fe6', api_secret='MHcCAQEEIMyQZh2rMhYSskz9CtGcP0ftDW2DjOyeGpga6CKZbTGloAoGCCqGSM49\nAwEHoUQDQgAEqev8+ouR6VwGQTvL4sPq5gRzNF9P871ZqV8MfohTGx7hS0YB6Fy1\nEW2ucn5rr0WZ/qY0nwrk/6uxCtkBdESSpw==')
+        self.client = coinbase.wallet.client.Client(api_key='3c0f8e2c-50c2-462a-b0fb-ff02e8b45331/apiKeys/7caa9d5f-f34c-4dde-be95-5409a7d94fe6', api_secret='EW2ucn5rr0WZ/qY0nwrk/6uxCtkBdESSpw==')
 
     def add_random_fraction(self):
         # Generate a random fraction between 0 and 1 (exclusive)
