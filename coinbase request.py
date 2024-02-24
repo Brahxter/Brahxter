ac5873554d38494c4e0434f74a9f86bc12720fb1
Brahxter <142146244+Brahxter@users.noreply.github.com>
2024-02-24 15:25:25 +0000

    modified:   coinbase request.py

--- coinbase request.py
+++ coinbase request.py
@@ -3,8 +3,8 @@ import requests
 import hashlib  # For password hashing
 
 
-api_key = "7caa9d5f-f34c-4dde-be95-5409a7d94fe6"
-api_secret = "\nMHcCAQEEIMyQZh2rMhYSskz9CtGcP0ftDW2DjOyeGpga6CKZbTGloAoGCCqGSM49\nAwEHoUQDQgAEqev8+ouR6VwGQTvL4sPq5gRzNF9P871ZqV8MfohTGx7hS0YB6Fy1\nEW2ucn5rr0WZ/qY0nwrk/6uxCtkBdESSpw==\n"
+api_key = "JJbzfgpOQe6I9myG"
+api_secret = "vYTWlUpjvJLWnVimEKsbtyHjxoOMEIzc"
 
 client = Client(api_key, api_secret)
 
